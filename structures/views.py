5973723c11c3a0a9c585f0cbefc7b9241fcc7048
Matteo Ghia <61846068+Maestro-Zacht@users.noreply.github.com>
2023-09-07 15:07:43 +0200

    add character api

--- structures/views.py
+++ structures/views.py
@@ -12,25 +12,18 @@ from django.db.models import Count, Q
 from django.http import HttpResponse, HttpResponseServerError, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from django.utils import translation
-from django.utils.html import format_html
 from django.utils.translation import gettext as _
 from esi.decorators import token_required
 from eveuniverse.core import eveimageserver
 from eveuniverse.models import EveType, EveTypeDogmaAttribute
 
 from allianceauth.authentication.models import CharacterOwnership
-from allianceauth.eveonline.models import EveCharacter, EveCorporationInfo
 from allianceauth.services.hooks import get_extension_logger
-from app_utils.allianceauth import notify_admins
 from app_utils.logging import LoggerAddTag
-from app_utils.messages import messages_plus
 from app_utils.views import image_html
 
-from . import __title__, tasks
+from . import __title__
 from .app_settings import (
-    STRUCTURES_ADMIN_NOTIFICATIONS_ENABLED,
-    STRUCTURES_DEFAULT_LANGUAGE,
     STRUCTURES_DEFAULT_PAGE_LENGTH,
     STRUCTURES_DEFAULT_TAGS_FILTER_ENABLED,
     STRUCTURES_PAGING_ENABLED,
@@ -43,7 +36,8 @@ from .core.serializers import (
     StructureListSerializer,
 )
 from .forms import TagsFilterForm
-from .models import Owner, Structure, StructureItem, StructureTag, Webhook
+from .models import Owner, Structure, StructureItem, StructureTag
+from .api import add_character, get_add_character_esi_scopes, get_add_character_permissions
 
 logger = LoggerAddTag(get_extension_logger(__name__), __title__)
 
@@ -425,108 +419,14 @@ def starbase_detail(request, structure_id):
 
 
 @login_required
-@permission_required("structures.add_structure_owner")
-@token_required(scopes=Owner.get_esi_scopes())  # type: ignore
+@permission_required(get_add_character_permissions())
+@token_required(scopes=get_add_character_esi_scopes())  # type: ignore
 def add_structure_owner(request, token):
     """View for adding or replacing a structure owner."""
-    token_char = get_object_or_404(EveCharacter, character_id=token.character_id)
     try:
-        character_ownership = CharacterOwnership.objects.get(
-            user=request.user, character=token_char
-        )
+        add_character(request, token)
     except CharacterOwnership.DoesNotExist:
-        character_ownership = None
-        messages_plus.error(
-            request,
-            format_html(
-                _(
-                    "You can only use your main or alt characters "
-                    "to add corporations. "
-                    "However, character %s is neither. "
-                )
-                % format_html("<strong>{}</strong>", token_char.character_name)
-            ),
-        )
-        return redirect("structures:index")
-    try:
-        corporation = EveCorporationInfo.objects.get(
-            corporation_id=token_char.corporation_id
-        )
-    except EveCorporationInfo.DoesNotExist:
-        corporation = EveCorporationInfo.objects.create_corporation(
-            token_char.corporation_id
-        )
-    owner, created = Owner.objects.update_or_create(
-        corporation=corporation, defaults={"is_active": True}
-    )
-    owner.add_character(character_ownership)
-    if created:
-        default_webhooks = Webhook.objects.filter(is_default=True)
-        if default_webhooks:
-            for webhook in default_webhooks:
-                owner.webhooks.add(webhook)
-            owner.save()
-
-    if owner.characters.count() == 1:
-        tasks.update_all_for_owner.delay(owner_pk=owner.pk, user_pk=request.user.pk)  # type: ignore
-        messages_plus.info(
-            request,
-            format_html(
-                _(
-                    "%(corporation)s has been added with %(character)s "
-                    "as sync character. "
-                    "We have started fetching structures and notifications "
-                    "for this corporation and you will receive a report once "
-                    "the process is finished."
-                )
-                % {
-                    "corporation": format_html("<strong>{}</strong>", owner),
-                    "character": format_html("<strong>{}</strong>", token_char),
-                }
-            ),
-        )
-        if STRUCTURES_ADMIN_NOTIFICATIONS_ENABLED:
-            with translation.override(STRUCTURES_DEFAULT_LANGUAGE):
-                notify_admins(
-                    message=_(
-                        "%(corporation)s was added as new "
-                        "structure owner by %(user)s."
-                    )
-                    % {"corporation": owner, "user": request.user.username},
-                    title=_("%s: Structure owner added: %s") % (__title__, owner),
-                )
-    else:
-        messages_plus.info(
-            request,
-            format_html(
-                _(
-                    "%(character)s has been added to %(corporation)s "
-                    "as sync character. "
-                    "You now have %(characters_count)d sync character(s) configured."
-                )
-                % {
-                    "corporation": format_html("<strong>{}</strong>", owner),
-                    "character": format_html("<strong>{}</strong>", token_char),
-                    "characters_count": owner.characters_count(),
-                }
-            ),
-        )
-        if STRUCTURES_ADMIN_NOTIFICATIONS_ENABLED:
-            with translation.override(STRUCTURES_DEFAULT_LANGUAGE):
-                notify_admins(
-                    message=_(
-                        "%(character)s was added as sync character to "
-                        "%(corporation)s by %(user)s.\n"
-                        "We now have %(characters_count)d sync character(s) configured."
-                    )
-                    % {
-                        "character": token_char,
-                        "corporation": owner,
-                        "user": request.user.username,
-                        "characters_count": owner.characters_count(),
-                    },
-                    title=_("%s: Character added to: %s") % (__title__, owner),
-                )
+        pass
     return redirect("structures:index")
 
 

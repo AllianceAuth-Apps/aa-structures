21648187feba227808c702c429962d42852adf7f
T'rahk Rokym <trahk.rokym@gmail.com>
2025-11-01 21:37:20 +0100

    Add commands to link to the metenox and moonmining libraries

---
+++ structures/thirdparty/moonmining.py
@@ -0,0 +1,23 @@
+"""
+Handles interactions with the metenox application
+
+https://gitlab.com/r0kym/aa-metenox
+"""
+
+from moonmining.models import Moon
+
+
+def get_athanor_moon_value_by_moon_id(moon_id: int) -> float:
+    """
+    Tries to return the monthly value of an athanor'd moon.
+    Returns 0 if the moon is unknown.
+    """
+
+    value = 0.0
+    try:
+        moon = Moon.objects.get(eve_mooon__id=moon_id)
+        value = moon.value
+    except Moon.DoesNotExist:
+        pass
+
+    return value
